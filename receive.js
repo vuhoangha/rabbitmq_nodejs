3e19eb16b564e8856d73366c8227a56839d04d9a
Vu hoang ha <ha.vu>
2018-03-18 16:14:58 +0700

    update code

--- receive.js
+++ receive.js
@@ -1,20 +1,36 @@
 #!/usr/bin/env node
 
 const amqp = require('amqplib/callback_api');
-const EXCHANGE = 'direct_logs';
-
-const severity = process.argv[2];
+const EXCHANGE = 'topic_logs';
 
 amqp.connect('amqp://guest:guest@192.168.99.100:5672', function (err, conn) {
     conn.createChannel(function (err, ch) {
-        ch.assertExchange(EXCHANGE, 'direct', { durable: false });
+        ch.assertExchange(EXCHANGE, 'topic', { durable: false });
 
         ch.assertQueue('', { exclusive: true }, function (err, q) {
             console.log(' [*] Waiting for logs. To exit press CTRL+C');
-            ch.bindQueue(q.queue, EXCHANGE, severity);
+            ch.bindQueue(q.queue, EXCHANGE, "hadang.*");
 
             ch.consume(q.queue, function (msg) {
-                console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+                console.log(" [x] %s:'%s'", "hadang.*", msg.content.toString());
+            }, { noAck: true });
+        });
+
+        ch.assertQueue('', { exclusive: true }, function (err, q) {
+            console.log(' [*] Waiting for logs. To exit press CTRL+C');
+            ch.bindQueue(q.queue, EXCHANGE, "*.info");
+
+            ch.consume(q.queue, function (msg) {
+                console.log(" [x] %s:'%s'", "*.info", msg.content.toString());
+            }, { noAck: true });
+        });
+
+        ch.assertQueue('', { exclusive: true }, function (err, q) {
+            console.log(' [*] Waiting for logs. To exit press CTRL+C');
+            ch.bindQueue(q.queue, EXCHANGE, "havu.warn*");
+
+            ch.consume(q.queue, function (msg) {
+                console.log(" [x] %s:'%s'", "havu.warn*", msg.content.toString());
             }, { noAck: true });
         });
     });
--- send.js
+++ send.js
@@ -1,30 +1,17 @@
 #!/usr/bin/env node
 
 const amqp = require('amqplib/callback_api');
-const EXCHANGE = 'direct_logs';
+const EXCHANGE = 'topic_logs';
 
-amqp.connect('amqp://guest:guest@192.168.99.100:5672', (err, conn) => {
-    conn.createChannel((err, ch) => {
-        ch.assertExchange(EXCHANGE, 'direct', { durable: false });
+amqp.connect('amqp://guest:guest@192.168.99.100:5672', function (err, conn) {
+    conn.createChannel(function (err, ch) {
+        ch.assertExchange(EXCHANGE, 'topic', { durable: false });
 
-        const send = time => {
-            console.log("\n\n");
-            const msg = new Date().toISOString();
-
-            ch.publish(EXCHANGE, 'info', new Buffer(msg));
-            console.log(" [x] Sent %s: '%s'", 'info', msg);
-
-            ch.publish(EXCHANGE, 'warning', new Buffer(msg));
-            console.log(" [x] Sent %s: '%s'", 'warning', msg);
-
-            ch.publish(EXCHANGE, 'error', new Buffer(msg));
-            console.log(" [x] Sent %s: '%s'", 'error', msg);
-
-            setTimeout(() => {
-                send(time);
-            }, time);
-        };
-
-        send(1000);
+        const key = process.argv[2];
+        const msg = `Hello World ${key}`;
+        ch.publish(EXCHANGE, key, new Buffer(msg));
+        console.log(" [x] Sent %s:'%s'", key, msg);
     });
+
+    setTimeout(function () { conn.close(); process.exit(0) }, 500);
 });
